65b256e0f25681029c739e0636d11d4a93c68aaf
Sviatlana Kruhlikava <kruhlikava@gmail.com>
2024-10-20 13:21:33 -0400

    Created new version of project for refactoring from scratch. Added .gitattributes. Removed .idea folder from tracking

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import { CALC_PADS } from "./dataStorage";
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-    <App data={ CALC_PADS }/>, document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
